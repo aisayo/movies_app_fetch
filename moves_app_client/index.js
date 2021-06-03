7ebcbc73bc9abf52b65c5496dd0e85f9b27b42dc
Aysan Isayo <aisayo@icloud.com>
2021-06-03 14:25:56 -0700

    solution

--- moves_app_client/index.js
+++ moves_app_client/index.js
@@ -1,135 +1,130 @@
-const moviesContainer = document.getElementById('movies-container')
+const moviesContainer = document.querySelector("#movies-container")
 const form = document.querySelector("#movies-form")
-const formContainer = document.getElementById('form-container')
-moviesContainer.addEventListener('click', handleMovieClick)
-formContainer.addEventListener('submit', handleSubmit)
+
+form.addEventListener('submit', handleSubmit)
+
+function getMovies(){
+    fetch('http://localhost:3000/movies')
+    .then(resp => resp.json())
+    .then(renderMovies)
+}
+
+function renderMovies(movies){
+    const moviesArr = movies.data
+    moviesArr.forEach((movie) => {
+        renderMovie(movie)
+    })
+}
+
+function renderMovie(movie){
+   const movieDetails = movie.attributes
+   moviesContainer.innerHTML += `
+    <div id=movie-${movieDetails.id} data-id=${movieDetails.id}>
+        <img class="movies-img" data-id=${movieDetails.id} src=${movieDetails.img}
+    </div>
+    <br>
+    <button>Edit</button>
+    <button>Delete</button>
+    <br>
+    <br>
+   `
+   const moviesImgs = document.getElementsByClassName('movies-img')
+
+    for (const img of moviesImgs){
+        img.addEventListener('click', getMovie)
+    }
+
+    const bttns = document.querySelectorAll('button')
+    for (const bttn of bttns){
+        bttn.addEventListener('click', handleBttnClick)
+    }
+}
+
+function getMovie(event){
+    const id = event.target.dataset.id
+    fetch(`http://localhost:3000/movies/${id}`)
+    .then(resp => resp.json())
+    .then(movie => {
+        renderMovieDetails(movie)
+        const backBttn = document.getElementById('back-bttn')
+        backBttn.addEventListener('click', goBack)
+    })
+}
+
+function renderMovieDetails(movie){
+    const movieDetails = movie.data.attributes
+    moviesContainer.innerHTML = ''
+    moviesContainer.innerHTML += `
+    <img data-id=${movieDetails.id} src=${movieDetails.img}><br>
+    Title: ${movieDetails.name}<br>
+    Release Year: ${movieDetails.release_year}<br>
+    Runtime: ${movieDetails.run_time}<br>
+    <a id="back-bttn" href="#">Back</a>
+    `
+
+}
+
+function goBack(e){
+    moviesContainer.innerHTML = ''
+    getMovies()
+}
 
 function handleSubmit(e){
     e.preventDefault()
 
+    // get form data 
+    // package it up to be sent to backend 
+
     const movieObj = {
-        name: document.getElementById('movie-name').value,
-        release_year: document.getElementById('movie-date').value,
-        run_time: document.getElementById('movie-run-time').value,
-        img: document.getElementById('movie-img').value,
+        name: document.querySelector("#movie-name").value,
+        release_year: document.querySelector("#movie-date").value,
+        run_time: document.querySelector("#movie-run-time").value,
+        img: document.querySelector("#movie-img").value
     }
 
     const options = {
         method: 'POST',
         headers: {
-            "Content-Type": "application/json",
-             Accept: "application/json"
+            "Content-Type": "application/json"
         },
         body: JSON.stringify(movieObj)
     }
 
     fetch('http://localhost:3000/movies', options)
     .then(resp => resp.json())
-    .then(data => {
-        const movie = data.data
-        renderMovie(movie)
+    .then(movie => {
+        const m = movie.data
+        renderMovie(m)
         clearForm()
-    })
+    }
+    )
 }
 
-function getMovies(){
-    fetch('http://localhost:3000/movies')
-    .then(resp => resp.json())
-    .then(movies => {
-        renderMovies(movies.data)
-    })
-}
-
-function renderMovies(movies){
-    movies.forEach((movie) => {
-        renderMovie(movie)
-    })
-}
-
-function renderMovie(movie){
-    moviesContainer.innerHTML += `
-        <div class="movie-item" id=${movie.attributes.id}>
-            <img 
-                id=${movie.attributes.id} 
-                src=${movie.attributes.img}
-            >
-            <button>Edit</button>
-            <button>Delete</button>
-        </div>
-    `
-    // moviesContainer.addEventListener('click', handleBttnClick)
-}
-
-function handleBttnClick(event){
-    if (event.target.innerHTML === "Delete"){
-        handleDelete(event.target)
-    } else if (event.target.innerHTML === 'Edit'){
-        handleEdit(event.target)
+function clearForm(){
+    for (const input of form.children){
+        if (input.type !== "submit"){
+            input.value = ""
+        }
     }
 }
 
-function handleDelete(element){
+function handleBttnClick(event){
+    if (event.target.innerText === 'Delete'){
+        deleteMovie(event.target)
+    } else if (event.target.innerText === 'Edit'){
+        // handle edit
+    }
+}
+
+function deleteMovie(element){
+    const id = element.parentElement.dataset.id
     element.parentElement.remove()
-    const id = element.parentElement.id
     fetch(`http://localhost:3000/movies/${id}`, {
         method: 'DELETE'
     })
     .then(resp => resp.json())
-    .then(data => {
-        alert(data["message"])
-    })
-}
-
-function handleMovieClick(event){
-    if (event.target.nodeName === 'IMG'){
-       getMovie(event.target.id)
-    } else if (event.target.innerHTML === "Delete"){
-        handleDelete(event.target)
-    } else if (event.target.innerHTML === 'Edit'){
-        handleEdit(event.target)
-}
-
-}
-
-function getMovie(movieId){
-    fetch(`http://localhost:3000/movies/${movieId}`)
-    .then(resp => resp.json())
-    .then(movie => {
-        form.remove()
-        let movieDetails = movie.data.attributes
-        renderMovieDetails(movieDetails)
-    })
-}
-
-function renderMovieDetails(movieDetails){
-    moviesContainer.innerHTML = `
-    <img src=${movieDetails.img}><br>
-    Movie Name: ${movieDetails.name}<br>
-    Release Date: ${movieDetails.release_year}<br>
-    Run Time: ${movieDetails.run_time}<br>
-    <br>
-    <a id="back" href=#>Back</a>
-`
-const backButton = document.getElementById('back')
-backButton.addEventListener('click', goBack)
-
-}
-
-function goBack(){
-    moviesContainer.innerHTML = ''
-    formContainer.append(form)
-    getMovies()
-}
-
-function clearForm(){
-    let inputArr = Array.from(formContainer.firstElementChild.children)
-    inputArr.forEach(input => { 
-        if (input.value !== "Create Movie"){
-            input.value = "" 
-        }
-    })
+    .then(data => alert(data.message))
 }
 
 
 getMovies()
-
