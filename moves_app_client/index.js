658d5c174897371de29f3385af36e64973fa9d42
Aysan Isayo <aisayo@icloud.com>
2021-06-03 16:19:52 -0700

    lecture

---
+++ moves_app_client/index.js
@@ -0,0 +1,85 @@
+const moviesContainer = document.querySelector("#movies-container")
+
+function getMovies(){
+    fetch('http://127.0.0.1:3000/movies')
+    .then(resp => resp.json())
+    .then(renderMovies)
+}
+
+function renderMovies(movies){
+    let moviesArr = movies.data
+    moviesArr.forEach(movie => {
+        renderMovie(movie)
+    })
+}
+
+function renderMovie(movie){
+    let movieDetails = movie.attributes
+    moviesContainer.innerHTML += `
+        <div data-id=${movieDetails.id}>
+            <img data-id=${movieDetails.id} class="movie-img" src=${movieDetails.img} />
+            <button>Edit</button>
+            <button>Delete</button>
+        </div>
+    `
+    const movieImgs = document.getElementsByClassName('movie-img')    
+    for (const img of movieImgs){
+        img.addEventListener('click', movieShow)
+    }
+
+    const bttns = document.querySelectorAll('button')
+    for (const bttn of bttns){
+        bttn.addEventListener('click', handleButton)
+    }
+}
+
+function movieShow(){
+    const id = event.target.dataset.id
+
+    fetch(`http://127.0.0.1:3000/movies/${id}`)
+    .then(resp => resp.json())
+    .then(movie => {
+        const movieDetails = movie.data.attributes
+        moviesContainer.innerHTML = ''
+        moviesContainer.innerHTML += `
+            <img src=${movieDetails.img} />
+            Title: ${movieDetails.name}
+            Release Date: ${movieDetails.release_year}
+            Run Time: ${movieDetails.run_time}
+            <a id="back-bttn" href="#">Back</a>
+        `
+
+        const backBttn = document.getElementById('back-bttn')
+        backBttn.addEventListener('click', goBack)
+    })
+}
+
+function goBack(){
+    moviesContainer.innerHTML = ''
+    getMovies()
+}
+
+function handleButton(event){
+   if (event.target.innerText === 'Delete'){
+       deleteMovie(event.target)
+   } else if (event.target.innerText === 'Edit'){
+       // do something with edit 
+   }
+}
+
+function deleteMovie(element){
+    const div = element.parentElement
+    const id = div.dataset.id
+    div.remove()
+    
+    fetch(`http://127.0.0.1:3000/movies/${id}`, {
+        method: 'DELETE'
+    })
+    .then(resp => resp.json())
+    .then(data => alert(data.message))
+}
+
+
+
+getMovies()
+
--- movies_app_api/app/controllers/movies_controller.rb
+++ movies_app_api/app/controllers/movies_controller.rb
@@ -1,2 +1,18 @@
 class MoviesController < ApplicationController
+
+    def index 
+        movies = Movie.all
+        render json: MovieSerializer.new(movies)
+    end 
+
+    def show 
+        movie = Movie.find_by_id(params[:id])
+        render json: MovieSerializer.new(movie)
+    end 
+
+    def destroy 
+        movie = Movie.find_by_id(params[:id])
+        movie.destroy 
+        render json: {message: "#{movie.name} was successfully deleted"}
+    end 
 end
