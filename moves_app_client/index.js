36659127cdfddc28c209981deaf20eb0a68bcfca
Aysan Isayo <aisayo@icloud.com>
2021-06-04 12:06:04 -0700

    post

--- moves_app_client/index.js
+++ moves_app_client/index.js
@@ -1,5 +1,7 @@
 const moviesContainer = document.querySelector("#movies-container")
-let moviesArr = []
+const createMovieBttn = document.getElementById('create-movie-bttn')
+const movieForm = document.querySelector("#movies-form")
+createMovieBttn.addEventListener('click', createMovie)
 
 function getMovies(){
     fetch('http://127.0.0.1:3000/movies')
@@ -8,7 +10,7 @@ function getMovies(){
 }
 
 function renderMovies(movies){
-    moviesArr = movies.data
+    let moviesArr = movies.data
     moviesArr.forEach(movie => {
         renderMovie(movie)
     })
@@ -26,6 +28,10 @@ function renderMovie(movie){
             <br>
         </div>
     `
+    elListners()
+}
+
+function elListners(){
     const movieImgs = document.getElementsByClassName('movie-img')    
     for (const img of movieImgs){
         img.addEventListener('click', movieShow)
@@ -81,6 +87,40 @@ function deleteMovie(element){
     })
     .then(resp => resp.json())
     .then(data => alert(data.message))
+}
+
+function collectFormData(){
+    const movieObj = {
+        name: document.getElementById('movie-name').value,
+        release_year: document.getElementById('movie-date').value,
+        run_time: document.getElementById('movie-run-time').value,
+        img: document.getElementById('movie-img').value
+    }
+    return movieObj
+}
+
+function createMovie(){
+    event.preventDefault()
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify(collectFormData())
+    }
+
+    // fetch by default makes get request 
+    fetch('http://127.0.0.1:3000/movies', options)
+    .then(resp => resp.json())
+    .then(movie => {
+        debugger
+        renderMovie(movie.data)
+        clearForm()
+    })
+}
+
+function clearForm(){
+    movieForm.reset()
 }
 
 getMovies()
--- movies_app_api/app/controllers/movies_controller.rb
+++ movies_app_api/app/controllers/movies_controller.rb
@@ -10,9 +10,25 @@ class MoviesController < ApplicationController
         render json: MovieSerializer.new(movie)
     end 
 
+    def create
+        movie = Movie.new(movie_params)
+
+        if movie.save 
+            render json: MovieSerializer.new(movie)
+        else 
+            render json: {error: "Movie not successfully saved"}
+        end 
+    end 
+
     def destroy 
         movie = Movie.find_by_id(params[:id])
         movie.destroy 
         render json: {message: "#{movie.name} was successfully deleted"}
+    end 
+
+    private
+
+    def movie_params 
+        params.require(:movie).permit(:name, :release_year, :run_time, :img)
     end 
 end
